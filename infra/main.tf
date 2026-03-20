529f7da3252e23b6c3542b1b50ccfd4f03416109
joemendez10-sufjan <joemendez10@gmail.com>
2026-03-20 16:45:22 -0500

    final working cloud deployment

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# ---------------- NETWORK ----------------
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
@@ -29,4 +31,95 @@ resource "aws_route" "internet_access" {
 resource "aws_route_table_association" "rta" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.rt.id
+}
+
+# ---------------- IAM (FIX FOR ERROR) ----------------
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ---------------- ECS ----------------
+
+resource "aws_ecs_cluster" "main" {
+  name = "nimbolingo-cluster"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "nimbolingo-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "nimbolingo"
+      image     = "844632827975.dkr.ecr.us-east-1.amazonaws.com/nimbolingo"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 5000
+          hostPort      = 5000
+        }
+      ]
+    }
+  ])
+}
+
+# ---------------- SECURITY GROUP ----------------
+
+resource "aws_security_group" "ecs_sg" {
+  name   = "ecs-security-group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ---------------- ECS SERVICE ----------------
+
+resource "aws_ecs_service" "app" {
+  name            = "nimbolingo-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = [aws_subnet.public.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_sg.id]
+  }
 }
